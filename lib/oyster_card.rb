91b83216fefb5369e799e1cd792b9f7f4ec9d6fd
nicola <nicolaccarroll@gmail.com>
2021-08-02 15:56:11 +0100

    ex 6 topping up

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,9 +1,19 @@
 class Oystercard
 
-attr_reader :balance
+  attr_reader :balance
+
+  MAX_AMOUNT = 90
+  DEFAULT_AMOUNT = 0
+
 
   def initialize
-    @balance = 0
+    @balance = DEFAULT_AMOUNT
+  end
+
+  def top_up(amount)
+    raise "Max amount exceeded" if (@balance + amount) > MAX_AMOUNT
+    
+    @balance += amount
   end
 
 end
