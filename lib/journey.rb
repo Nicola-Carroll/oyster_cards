220bcd5311bc5939b89212ce2517853aef2e2ba9
nicola <nicolaccarroll@gmail.com>
2021-08-06 15:15:42 +0100

    last commit

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,5 +1,4 @@
 class Journey
-  attr_reader :entry_station, :exit_station
 
   def initialize(entry_station=nil, exit_station=nil)
     @entry_station = entry_station
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,7 +1,9 @@
 require './lib/journey'
 
 describe Journey do
-  
+  let(:station) { double :station }
+  subject(:complete_journey) { described_class.new(station, station)}
+
   it 'is an instance of' do
     expect(subject).to be_instance_of(Journey)
   end
@@ -9,20 +11,19 @@ describe Journey do
   describe '#complete?' do
 
     it 'incomplete if entry station missing' do
-      journey  = Journey.new(nil,'Bethnal Green')
+      journey  = Journey.new(nil,station)
       expect(journey).not_to be_complete
     end
 
 
     it 'incomplete if exit station missing' do
-      journey  =  Journey.new('Bethnal Green')
+      journey  =  Journey.new(station)
       expect(journey).not_to be_complete
     end
 
 
     it 'complete if neither station missing' do
-      journey  = Journey.new('Bethnal Green', 'Liv St')
-      expect(journey).to be_complete
+      expect(complete_journey).to be_complete
     end
 
   end
@@ -32,15 +33,14 @@ describe Journey do
     context 'journey complete' do
       
       it 'cost is minimum' do
-        journey  = Journey.new('Bethnal Green', 'Liv St')
-        expect(journey.fare).to eq 1
+        expect(complete_journey.fare).to eq 1
       end
     end
     
     context 'journey incomplete' do
 
       it 'cost is max' do
-        journey  = Journey.new('Bethnal Green')
+        journey  = Journey.new(station)
         expect(journey.fare).to eq 6
       end
 
